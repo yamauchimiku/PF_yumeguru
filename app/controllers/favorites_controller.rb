1dd1fabebdf5bd370837fdb15730623a4a8ec7e2
yamauchimiku <yamamochi7171@gmail.com>
2021-11-12 11:26:19 +0000

    [add] hidouki

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,16 @@
 class FavoritesController < ApplicationController
   def  create
-    post_image = PostImage.find(params[:post_image_id])
+    @post_image = PostImage.find(params[:post_image_id])
     # idを二つ指定
-    favorite = current_user.favorites.new(post_image_id: post_image.id)
+    favorite = current_user.favorites.new(post_image_id: @post_image.id)
     favorite.save
-    # 同じリダイレクト先
-    redirect_to request.referer
+    # app/views/favorites/create.js.erbを参照する
   end
 
   def destroy
-    post_image = PostImage.find(params[:post_image_id])
-    favorite = current_user.favorites.find_by(post_image_id: post_image.id)
+    @post_image = PostImage.find(params[:post_image_id])
+    favorite = current_user.favorites.find_by(post_image_id: @post_image.id)
     favorite.destroy
-    redirect_to request.referer
+    # app/views/favorites/destroy.js.erbを参照する
   end
 end
