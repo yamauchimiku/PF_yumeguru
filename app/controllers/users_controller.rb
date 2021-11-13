f7d74a31821493cd090fb5e778d03dbd4d0c2103
yamauchimiku <yamamochi7171@gmail.com>
2021-11-13 07:23:22 +0000

    [add] users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 class UsersController < ApplicationController
   def  show
     @user = User.find(params[:id])
-    @post_image = PostImage.find(params[:id])
+    # ユーザーに関連付けられた投稿のみ表示
+    @post_images = @user.post_images.page(params[:page]).reverse_order
   end
 
   def  edit
+    @user = User.find(params[:id])
   end
 
   def  update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
